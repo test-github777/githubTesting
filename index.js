1a03da183a394aef4c80bcb208c3189403bf518d
test-github777 <beysballer@mail.ru>
2021-02-03 20:06:51 +0300

    Testing

---
+++ index.js
@@ -0,0 +1,10 @@
+import User from 'User'
+const [Ez, setEz] = useState()
+const [asdf, setAsdf] = useState([])
+import React from 'react'
+
+function index() {
+  return <div>asdf</div>
+}
+
+export default index
